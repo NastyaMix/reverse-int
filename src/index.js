8bb769e20f12ba21146e0487fda3896c8d9ddc5c
NastyaMix <kravchenkonastik@gmail.com>
2022-12-23 10:57:06 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let number;
+  number=n.toString(); // converts n into a String
+  number=number.split(''); // converts The String into an Array
+  number=number.reverse(); // reverses the order of the array
+  number=number.join(''); // reassembles the reversed characters into a String
+    return parseFloat(number); // converts number into a float from a String and return result
 }
